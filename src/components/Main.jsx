124102548100c5027eb938742588196cac59a655
kylemucha <kylemuchaswe@gmail.com>
2025-12-19 00:42:09 -0800

    made some changes and learned about controlled components and how to display each keystroke as an action

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,22 @@
+import { useState } from"react" 
+
 export default function Main() {
+
+    const [meme, setMeme] = useState({
+        topText: "One Does not Simply",
+        bottomText: "Walk into these nuts",
+        imageURL: "http://i.imgflip.com/1bij.jpg" 
+    })
+
+    function handleChange(event) {
+        const { name, value } = event.currentTarget
+
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name] : value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -7,6 +25,7 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
                     />
                 </label>
 
@@ -15,14 +34,15 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageURL}/>
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
